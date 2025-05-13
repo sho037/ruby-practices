b380dfd6a93ce6814b9278d2099f9254f5819fb3
sho037 <103867061+sho037@users.noreply.github.com>
2025-05-13 10:08:20 +0900

    refactor: 場所を入れ替え

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -14,17 +14,13 @@ month = options["m"].to_i
 year = options["y"].to_i
 month = now.month if month == 0
 year = now.year if year == 0
+first_day = Date.new(year, month)
+last_day = Date.new(year, month, -1)
+first_day_of_week = first_day.strftime('%w').to_i
 
 first_item = []
 first_item.push("\t#{month}月 #{year}")
 first_item.push("日 月 火 水 木 金 土")
-
-first_day = Date.new(year, month)
-last_day = Date.new(year, month, -1)
-
-puts first_item
-
-first_day_of_week = first_day.strftime('%w').to_i
 
 def convert_day(day, space_time)
   day_length = day.to_s.length
@@ -35,13 +31,13 @@ def convert_day(day, space_time)
   day_ += day.to_s
 end
 
+puts first_item
 print "  " if first_day_of_week != 0
 (2..first_day_of_week).each do
   print "   "
 end
 
 week_index = first_day_of_week
-
 (1..last_day.day).each do | day_index |
   print convert_day(day_index, 3) if week_index != 0
   print convert_day(day_index, 2) if week_index == 0
