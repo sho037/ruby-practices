ab48ecac3fb1ea65f88a298824e12ca811e9d6b7
sho037 <103867061+sho037@users.noreply.github.com>
2025-05-13 14:43:57 +0900

    format: rubocop

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,54 +1,56 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'optparse'
 require 'date'
 
 options = ARGV.getopts('m:', 'y:')
 
-if options["m"].nil? && options["y"]
-  puts "y オプションのみは対応していません" 
+if options['m'].nil? && options['y']
+  puts 'y オプションのみは対応していません'
   return
 end
 
 now = Date.today
-month = options["m"].to_i
-year = options["y"].to_i
-month = now.month if month == 0
-year = now.year if year == 0
+month = options['m'].to_i
+year = options['y'].to_i
+month = now.month if month.zero?
+year = now.year if year.zero?
 first_day = Date.new(year, month)
 last_day = Date.new(year, month, -1)
 first_day_of_week = first_day.strftime('%w').to_i
-is_this_month = month == now.month ? true : false
+is_this_month = month == now.month
 print_stack = []
 
 def convert_day(day, space_time, is_today)
   day_length = day.to_s.length
-  day_ = ""
+  day_ = ''
   (1..(space_time - day_length)).each do
-    day_ += " "
+    day_ += ' '
   end
-  day_ += is_today ? "\e[7m#{day.to_s}\e[0m" : day.to_s
+  day_ += is_today ? "\e[7m#{day}\e[0m" : day.to_s
 end
 
 # 最初の表示
 print_stack.push("      #{month}月 #{year}")
-print_stack.push("日 月 火 水 木 金 土")
+print_stack.push('日 月 火 水 木 金 土')
 
 # 月初めの空白
-print_stack[2] = "  " if first_day_of_week != 0
+print_stack[2] = '  ' if first_day_of_week != 0
 (2..first_day_of_week).each do
-  print_stack[2] += "   "
+  print_stack[2] += '   '
 end
 
 # 日にち
 for_is_today = is_this_month ? now.day : 0
-stack_index = 2 
+stack_index = 2
 week_index = first_day_of_week
-(1..last_day.day).each do | day_index |
-  print_stack[stack_index] ||= ""
-  print_stack[stack_index] += convert_day(day_index, week_index == 0 ? 2 : 3, for_is_today == day_index)
-  
+(1..last_day.day).each do |day_index|
+  print_stack[stack_index] ||= ''
+  print_stack[stack_index] += convert_day(day_index, week_index.zero? ? 2 : 3, for_is_today == day_index)
+
   week_index += 1
-  if week_index % 7 == 0
+  if (week_index % 7).zero?
     stack_index += 1
     week_index = 0
   end
