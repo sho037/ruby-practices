a28c1fef86b028582cf3afe8421ff282413b975f
sho037 <103867061+sho037@users.noreply.github.com>
2025-05-14 13:47:13 +0900

    feat: ボウリングの計算ができるように

---
+++ 03.bowling/bowling.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+arg_scores = ARGV[0].split(',')
+
+def convert_to_int(str_array)
+  str_array.map do |s|
+    s.eql?('X') ? 10 : s.to_i
+  end
+end
+
+arg_scores = convert_to_int(arg_scores)
+
+final_score = 0
+score_index = 0
+flame_index = 0
+while flame_index < 10
+  if arg_scores[score_index].eql?(10)
+    # ストライク
+    final_score += 10 + arg_scores[score_index + 1] + arg_scores[score_index + 2]
+    score_index += 1
+  elsif arg_scores[score_index] + arg_scores[score_index + 1] == 10
+    # スペア
+    final_score += arg_scores[score_index] + arg_scores[score_index + 1] + arg_scores[score_index + 2]
+    score_index += 2
+  else
+    final_score += arg_scores[score_index] + arg_scores[score_index + 1]
+    score_index += 2
+  end
+  flame_index += 1
+end
+
+puts final_score
