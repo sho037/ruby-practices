25a117c341d4ef5cc1723971fc0fb15946638126
sho037 <103867061+sho037@users.noreply.github.com>
2025-05-19 10:04:26 +0900

    refactor: フラグを削除

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,17 +1,14 @@
 def print_fizzbuzz(start, finish)
   (start..finish).each do | index |
-    print_index_flag = true
     if index % 3 == 0
       print "Fizz"
-      print_index_flag = false
     end
 
     if index % 5 == 0
       print "Buzz"
-      print_index_flag = false
     end
     
-    print index if print_index_flag
+    print index if index % 3 != 0 && index % 5 != 0
     puts
   end
 end
