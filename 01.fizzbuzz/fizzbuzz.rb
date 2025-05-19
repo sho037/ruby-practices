9d80bee4b972cfefd42ef07b6cd625aa62ed4173
sho037 <103867061+sho037@users.noreply.github.com>
2025-05-19 10:05:41 +0900

    format: rubocop

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,13 +1,11 @@
-def print_fizzbuzz(start, finish)
-  (start..finish).each do | index |
-    if index % 3 == 0
-      print "Fizz"
-    end
+# frozen_string_literal: true
 
-    if index % 5 == 0
-      print "Buzz"
-    end
-    
+def print_fizzbuzz(start, finish)
+  (start..finish).each do |index|
+    print 'Fizz' if (index % 3).zero?
+
+    print 'Buzz' if (index % 5).zero?
+
     print index if index % 3 != 0 && index % 5 != 0
     puts
   end
