b2f147e870264c0e3d443bc37dc53e5360b79eea
sho037 <103867061+sho037@users.noreply.github.com>
2025-05-20 11:57:12 +0900

    refactor: 判定方法を変更

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -4,7 +4,7 @@
 arg_scores = ARGV[0].split(',')
 
 def convert_to_int(str_array)
-  str_array.map { |s| s.eql?('X') ? 10 : s.to_i }
+  str_array.map { |s| s == 'X' ? 10 : s.to_i }
 end
 
 arg_scores = convert_to_int(arg_scores)
@@ -13,7 +13,7 @@ final_score = 0
 score_index = 0
 frame_index = 0
 while frame_index < 10
-  if arg_scores[score_index].eql?(10)
+  if arg_scores[score_index] == 10
     # ストライク
     final_score += 10 + arg_scores[score_index + 1] + arg_scores[score_index + 2]
     score_index += 1
