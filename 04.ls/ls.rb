2e65c7790f4df53862c1fb30bd4369a349831dba
sho037 <103867061+sho037@users.noreply.github.com>
2025-05-26 18:12:05 +0900

    feat: ディレクトリとファイルが表示されるように

---
+++ 04.ls/ls.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+MAXIMUM_COLUMN = 3
+
+dir_and_files = Dir.glob("*").sort
+
+# 何行必要か
+how_many_rows = dir_and_files.length / MAXIMUM_COLUMN
+how_many_rows += 1 unless dir_and_files.length % MAXIMUM_COLUMN == 0
+
+# 列ごとの最大文字数
+max_num_per_column = []
+
+daf_index = 0
+MAXIMUM_COLUMN.times do |index|
+  max_num_per_column[index] ||= 0
+  how_many_rows.times do
+    next unless dir_and_files[daf_index]
+    max_num_per_column[index] = dir_and_files[daf_index].length if max_num_per_column[index] < dir_and_files[daf_index].length
+    daf_index += 1
+  end
+end
+
+rows = []
+daf_index = 0
+MAXIMUM_COLUMN.times do |max_c_index|
+  how_many_rows.times do |index|
+    next unless dir_and_files[daf_index]
+    rows[index] ||= ""
+    rows[index] = rows[index] + dir_and_files[daf_index].ljust(max_num_per_column[max_c_index]) + "\t"
+    daf_index += 1
+  end
+end
+
+puts rows
