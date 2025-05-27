aec0b69a75944353dee91c9babc7b5f1f74823c7
sho037 <103867061+sho037@users.noreply.github.com>
2025-05-27 16:23:05 +0900

    refactor: 取得した要素を使って繰り返しを回すように

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -11,14 +11,13 @@ max_str_num = 0
 dir_and_files.each { |daf| max_str_num = daf.length if daf.length > max_str_num }
 
 rows = []
-daf_index = 0
-MAX_COLUMNS.times do
-  how_many_rows.times do |index|
-    next unless dir_and_files[daf_index]
-    rows[index] ||= ""
-    rows[index] = rows[index] + dir_and_files[daf_index].ljust(max_str_num) + "\t"
-    daf_index += 1
-  end
+row_index = 0
+dir_and_files.each do |daf|
+  row_index = 0 if row_index % how_many_rows == 0
+  
+  rows[row_index] ||= ""
+  rows[row_index] = rows[row_index] + daf.ljust(max_str_num) + "\t"
+  row_index += 1
 end
 
 puts rows
