fc3befbaa2277ebf7ecdaeb030dc548edbdade39
sho037 <103867061+sho037@users.noreply.github.com>
2025-06-02 18:02:37 +0900

    refactor: rubocop

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -3,7 +3,7 @@
 
 MAX_COLUMNS = 3
 
-dir_and_files = Dir.glob("*").sort
+dir_and_files = Dir.glob('*').sort
 
 how_many_rows = (dir_and_files.length.to_f / MAX_COLUMNS).ceil
 
@@ -13,10 +13,10 @@ dir_and_files.each { |daf| max_str_num = daf.length if daf.length > max_str_num 
 rows = []
 row_index = 0
 dir_and_files.each do |daf|
-  row_index = 0 if row_index % how_many_rows == 0
-  
-  rows[row_index] ||= ""
-  rows[row_index] = rows[row_index] + daf.ljust(max_str_num) + "\t"
+  row_index = 0 if (row_index % how_many_rows).zero?
+
+  rows[row_index] ||= ''
+  rows[row_index] = "#{rows[row_index]}#{daf.ljust(max_str_num)}\t"
   row_index += 1
 end
 
