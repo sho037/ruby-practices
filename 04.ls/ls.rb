95fce34bb9f64ce29b70027cf563a40989da84de
sho037 <103867061+sho037@users.noreply.github.com>
2025-06-03 11:04:46 +0900

    refactor: 関数化

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -3,21 +3,29 @@
 
 MAX_COLUMNS = 3
 
-dir_and_files = Dir.glob('*').sort
+def main
+  dir_and_files = Dir.glob('*').sort
 
-rows_count = (dir_and_files.length.to_f / MAX_COLUMNS).ceil
+  rows_count = (dir_and_files.length.to_f / MAX_COLUMNS).ceil
+  max_str_num = 0
+  dir_and_files.each { |daf| max_str_num = daf.length if daf.length > max_str_num }
 
-max_str_num = 0
-dir_and_files.each { |daf| max_str_num = daf.length if daf.length > max_str_num }
-
-rows = []
-row_index = 0
-dir_and_files.each do |daf|
-  row_index = 0 if (row_index % rows_count).zero?
-
-  rows[row_index] ||= ''
-  rows[row_index] = "#{rows[row_index]}#{daf.ljust(max_str_num)}\t"
-  row_index += 1
+  puts format_columns_by_row_count(dir_and_files, rows_count, max_str_num)
 end
 
-puts rows
+def format_columns_by_row_count(items, rows_count, column_width)
+  rows = []
+  row_index = 0
+
+  items.each do |item|
+    row_index = 0 if (row_index % rows_count).zero?
+
+    rows[row_index] ||= ''
+    rows[row_index] += "#{item.ljust(column_width)}\t"
+    row_index += 1
+  end
+
+  rows
+end
+
+main if __FILE__ == $PROGRAM_NAME
